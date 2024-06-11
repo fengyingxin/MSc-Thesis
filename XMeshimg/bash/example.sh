ec3fc64ed3d56ec580b8ef9390d24b543ff61e94
fengyingxin <58913029+fengyingxin@users.noreply.github.com>
2024-06-11 19:57:37 +0200

    Add files via upload

---
+++ XMeshimg/bash/example.sh
@@ -0,0 +1,30 @@
+python image_xmesh.py \
+    --obj_path ../Data/castle.obj \
+    --output_dir ./outputs/castle \
+    --prompt "" \
+    --sigma 12.0 \
+    --clamp tanh \
+    --n_normaugs 4 \
+    --n_augs 1 \
+    --normmincrop 0.1 \
+    --normmaxcrop 0.4 \
+    --geoloss \
+    --colordepth 2 \
+    --normdepth 2 \
+    --frontview \
+    --frontview_std 4 \
+    --clipavg view \
+    --lr_decay 0.9 \
+    --clamp tanh \
+    --normclamp tanh \
+    --save_render \
+    --seed 23 \
+    --n_iter 800 \
+    --normal_learning_rate 0.0005 \
+    --standardize \
+    --no_pe \
+    --symmetry \
+    --background 1 1 1 \
+    --normratio 0.1 \
+    --prompt_choice "image" \
+    --prompt_image "./prompt_image/castle.jpg" 
